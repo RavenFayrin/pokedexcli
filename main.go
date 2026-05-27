af6ab1d712886a9ca7f13f017c2ecbe25942625b
RavenFayrin <raven.fayrin@gmail.com>
2026-05-27 18:22:48 -0400

    Made to match lesson

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			text := scanner.Text()
-			words := cleanInput(text)
-			fmt.Println("Your command was:", words[0])
-		}
-
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
+	startRepl()
 }
