ce77aea0d1803a100096f2c8a2be0178cfc92241
RavenFayrin <raven.fayrin@gmail.com>
2026-06-04 18:20:32 -0400

    made to match lesson

--- command_inspect.go
+++ command_inspect.go
@@ -11,14 +11,21 @@ func commandInspect(cfg *config, args ...string) error {
 	}
 
 	name := args[0]
+	pokemon, ok := cfg.caughtPokemon[name]
+	if !ok {
+		return errors.New("you have not caught that pokemon")
+	}
 
-	pokemon, exists := cfg.caughtPokemon[name]
-	if exists {
-		fmt.Printf("Name: %s\n", pokemon.Name)
-		fmt.Printf("Height: %d\n", pokemon.Height)
-		fmt.Printf("Weight: %d\n", pokemon.Weight)
-	} else {
-		fmt.Println("you have not caught that pokemon")
+	fmt.Println("Name:", pokemon.Name)
+	fmt.Println("Height:", pokemon.Height)
+	fmt.Println("Weight:", pokemon.Weight)
+	fmt.Println("Stats:")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf("  -%s: %v\n", stat.Stat.Name, stat.BaseStat)
+	}
+	fmt.Println("Types:")
+	for _, typeInfo := range pokemon.Types {
+		fmt.Println("  -", typeInfo.Type.Name)
 	}
 	return nil
 }
--- repl.go
+++ repl.go
@@ -71,15 +71,15 @@ func getCommands() map[string]cliCommand {
 			description: "Attempt to catch a pokemon",
 			callback:    commandCatch,
 		},
+		"inspect": {
+			name:        "inspect <pokemon_name>",
+			description: "View details about a caught Pokemon",
+			callback:    commandInspect,
+		},
 		"explore": {
 			name:        "explore <location_name>",
 			description: "Explore a location",
 			callback:    commandExplore,
-		},
-		"inspect": {
-			name:        "inspect <pokemon_name>",
-			description: "Learn about your pokemon",
-			callback:    commandInspect,
 		},
 		"map": {
 			name:        "map",
