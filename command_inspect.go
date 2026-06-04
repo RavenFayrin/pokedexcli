77473f84fbea17c5086e62328fec3c692a209ad2
RavenFayrin <raven.fayrin@gmail.com>
2026-06-04 18:18:20 -0400

    added inspect command

---
+++ command_inspect.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func commandInspect(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a pokemon name")
+	}
+
+	name := args[0]
+
+	pokemon, exists := cfg.caughtPokemon[name]
+	if exists {
+		fmt.Printf("Name: %s\n", pokemon.Name)
+		fmt.Printf("Height: %d\n", pokemon.Height)
+		fmt.Printf("Weight: %d\n", pokemon.Weight)
+	} else {
+		fmt.Println("you have not caught that pokemon")
+	}
+	return nil
+}
